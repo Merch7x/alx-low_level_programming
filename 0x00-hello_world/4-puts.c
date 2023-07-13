129fce3ba7b51d26315737fee06ed02ec43ed248
Merch7x <aquamaingi@gmail.com>
2023-07-13 12:11:34 +0300

    Print to stdout using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - prints to stdout using puts
+ * Return: returns an integer value
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
