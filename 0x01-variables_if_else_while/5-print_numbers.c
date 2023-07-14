365531aeb69fbdc194c9ae197684ce100c7a7a0b
Merch7x <aquamaingi@gmail.com>
2023-07-14 13:41:37 +0300

    Use putchar to output characters

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - output a value to stdout
+ * Return: returns an integer value
+ */
+int main(void)
+{
+	char a = '0';
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
