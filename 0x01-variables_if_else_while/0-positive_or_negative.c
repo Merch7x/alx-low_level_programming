3af0b99230526042e69b2fa2654efab01f5705db
Merch7x <aquamaingi@gmail.com>
2023-07-14 10:57:25 +0300

    Add if else statements to code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - generate a random int
+ * Return: returns an integer value
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%u is positive\n", n);
+	}
+	else if(n == 0)
+	{
+		printf("%u is zero\n", n);
+	}
+	else 
+	{
+		printf("%d is negative\n", n);
+	}
+	
+	return (0);
+}
