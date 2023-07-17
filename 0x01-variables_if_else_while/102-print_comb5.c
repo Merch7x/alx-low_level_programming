3be58e933b5c009a0fcc278dd74ba900e4499bff
Merch7x <aquamaingi@gmail.com>
2023-07-17 12:57:04 +0300

    Intialize code

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - uses putchar to output a given input
+ * Return: returns an integer value
+ */
+int main(void)
+{
+	int num = '0';
+	int space = ' ';
+	int comma = ',';
+	int num2 = '0';
+	int num3 = '0';
+	int num4 = '0';
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			for (num3 = '0'; num3 <= '9'; num3++)
+			{
+				for (num4 = '0'; num4 <= '9'; num4++)
+				{
+					if (num3 + num4 > num + num2)
+					{
+						putchar(num);
+						putchar(num2);
+						putchar(space);
+						putchar(num3);
+						putchar(num4);
+
+						if (num >= '0' && num <= '9')
+						{
+							putchar(comma);
+							putchar(space);
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+	putchar('\n');
+
+	return (0);
+}
