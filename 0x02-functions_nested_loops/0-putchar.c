2b7dfecd2d7c49c5bf71a02c3622d8bd20523da0
Merch7x <aquamaingi@gmail.com>
2023-07-18 19:50:22 +0300

    Output to stdout by calling a function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * main - output chars as stdout by calling a function
+ * Return: an integer value of 1 if successful
+ */
+int main(void)
+{
+	char a, b, c, d, e, f, g, h, i;
+
+	a = '_';
+	b = 'p';
+	c = 'u';
+	d = 't';
+	e = 'c';
+	f = 'h';
+	g = 'a';
+	h = 'r';
+	i = '\n';
+	_putchar(a);
+	_putchar(b);
+	_putchar(c);
+	_putchar(d);
+	_putchar(e);
+	_putchar(f);
+	_putchar(g);
+	_putchar(h);
+	_putchar(i);
+	return (0);
+}
