7c930379f66d9c6617871d4248ef0df7f23b532e
Merch7x <aquamaingi@gmail.com>
2023-07-19 00:03:26 +0300

    Output time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - prints out chars to stdout
+ * Return: no return value
+ */
+void jack_bauer(void)
+{
+	int a, b, c = ':', d, e, f = '\n';
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		for (b = '0'; b <= '3'; b++)
+		{
+			for (d = '0'; d <= '5'; d++)
+			{
+				for (e = '0'; e <= '9'; e++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(c);
+					_putchar(d);
+					_putchar(e);
+					_putchar(f);
+				}
+			}
+		}
+	}
+}
+
