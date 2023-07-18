11620bc22cd9525bc1034f4923e36df6b87641cb
Merch7x <aquamaingi@gmail.com>
2023-07-19 00:31:07 +0300

    Use functions for arithmetic operations

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - evaluates two parameters
+ * @a: requires an integer value
+ * @b: requires an integer value
+ * Return: value of evaluation
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
+
