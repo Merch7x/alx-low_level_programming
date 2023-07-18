8c265152c2b8ca79bdff474fed3d9238b5d3d3e2
Merch7x <aquamaingi@gmail.com>
2023-07-18 23:44:40 +0300

    Return last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+/**
+ * print_last_digit - evaluates an int parameter
+ * @a: requires an int value
+ * Return: returns value of the result
+ */
+int print_last_digit(int a)
+{
+	int result;
+
+	result = a % 10;
+	return (result);
+}
