013ecf786bca98b9af859794fa5544c532e0d120
Merch7x <aquamaingi@gmail.com>
2023-07-18 20:24:35 +0300

    Fix compiler error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,5 @@
 #include "main.h"
 /**
- * print_alphabet - prints lowercase alphabet to stdout
- * Return: does not return anything
- */
-void print_alphabet(void)
-{
-	char a, b = '\n';
-
-	for (a = 'a'; a <= 'z'; a++)
-	{
-		_putchar(a);
-	}
-
-	_putchar(b);
-}
-/**
  * main - outputs th result of print_alphabet
  * Return: an integer value
  */
