f8a1d7e1bbe02f76b9dff1a13141d3095d59f439
Merch7x <aquamaingi@gmail.com>
2023-07-19 17:35:46 +0300

    Use while loops to pritn to stdout

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - outs to numbers to stdout
+ * @n: a parameter of type int
+ * Return: no return value
+ */
+void print_to_98(int n)
+{
+	int a = n, b = 98;
+
+	if (a <= b)
+	{
+		while (a <= b)
+		{	
+			printf("%d", a);
+		
+			a++;
+
+			if ( a <= b)
+			{
+				printf(", ");
+			}
+		}
+	}
+
+	else if (a > b)
+	{
+		while (a > b)
+		{
+			printf("%d", a);
+			a--;
+
+			if ( a >= b)
+			{
+				printf(", ");
+			}
+		}
+	}
+
+	printf("\n");
+
+
+}
+
+
