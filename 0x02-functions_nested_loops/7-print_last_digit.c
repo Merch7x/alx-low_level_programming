e75a9b84099a0000791d54d4771de145d94dd8e8
Merch7x <aquamaingi@gmail.com>
2023-07-19 11:58:16 +0300

    Modify output

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * print_last_digit - evaluates an int parameter
  * @a: requires an int value
@@ -7,15 +8,14 @@ int print_last_digit(int a)
 {
 	int result;
 
-	if (a < 0)
-	{
-		result = (a % 10) * -1;
-	}
-	else if (a >= 0)
-	{
-		result = a % 10;
-	}
+	result = a % 10;
 
+	if (result < 0)
+	{
+		result = -result;
+	}
+	
+	_putchar(result + '0');
 	return (result);
 
 }
