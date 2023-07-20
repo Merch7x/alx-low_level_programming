ccd97e58b3fbf80f23b807b081287891a0ed390e
Merch7x <aquamaingi@gmail.com>
2023-07-21 01:47:06 +0300

    Output chars to stdout

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_line - outputs a char to stdout
+ * @n: parameter of integer value
+ * Return: no return value
+ */
+void print_line(int n)
+{
+	char c = '_';
+	int a = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (a <= n)
+		{
+			_putchar(c);
+			a++;
+		}
+		_putchar('\n');
+	}
+}
+
