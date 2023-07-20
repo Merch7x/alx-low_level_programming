17a3095b79e0b0c5e8152c0274b44f89d3d08d47
Merch7x <aquamaingi@gmail.com>
2023-07-20 22:45:09 +0300

    Compute multiples of two parameters

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiplies two parameters
+ * @a: integer parameter
+ * @b: integer parameter
+ * Return: returns an integer value
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
