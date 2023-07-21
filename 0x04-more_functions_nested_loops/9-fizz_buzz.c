d538c5c7509a1a83e974fcc7c57440f2646b2676
Merch7x <aquamaingi@gmail.com>
2023-07-21 17:55:14 +0300

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - outputs a list of numbers to stdout
+ * Return: returns an int value
+int main(void)
+{
+	int a = 1;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", a);
+		}
+	}
+	printf("\n");
+	return (0);
+}
