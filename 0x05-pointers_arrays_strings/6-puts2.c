2637a2c00a80c0dd7c952b1a5a9e4ae4c62ec750
Merch7x <aquamaingi@gmail.com>
2023-07-24 16:17:11 +0300

    Print even parts of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints chars to stdout
+ * @str: pointer parameter of char value
+ * Return: returns nothing
+ */
+void puts2(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (str[index] % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+		index++;
+	}
+	_putchar('\n');
+}
+
