0c85cebb46e2afde9d291bcf17802acf8667e522
Merch7x <aquamaingi@gmail.com>
2023-07-24 17:10:08 +0300

    Print array to stdout

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints arrays to stdout
+ * @a: parameter pointer
+ * @n: integer value
+ * Return: nothing to return
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		printf("%d", a[i]);
+		i++;
+
+		if (i < n)
+		{
+			printf(",");
+			printf(" ");
+		}
+	}
+	printf("\n");
+}
+
