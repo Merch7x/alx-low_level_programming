dd4b83ae2b5cc45990a98cadcf50c59edd862a3f
Merch7x <aquamaingi@gmail.com>
2023-07-26 19:31:46 +0300

    Change while to for loop

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,11 @@ void reverse_array(int *a, int n)
 	int i = 0;
 	int t;
 
-	while (i < n / 2)
+	for (i = 0; i < n / 2; i++)
 	{
 		t = a[i];
 		a[i] = a[n - 1];
 		a[n - 1] = t;
-		i++;
 		n--;
 	}
 }
