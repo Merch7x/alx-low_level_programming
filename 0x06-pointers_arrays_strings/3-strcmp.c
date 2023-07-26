783a53c15b1f9234be3f22880ab25a8b8147fda3
Merch7x <aquamaingi@gmail.com>
2023-07-26 08:30:53 +0300

    Compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: an int value of the comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0, len = 0;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	len = i + j;
+
+	for (i = 0; i < len; i++)
+	{
+		if (s1[i] < s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		else if (s1[i] > s2[i])
+		{
+			return (s1[i]- s2[i]);
+		}
+	}
+	return (0);
+}
+
