8b6ae7a7a0a917456ab224e4b0ae776e72e492a6
Merch7x <aquamaingi@gmail.com>
2023-08-01 00:32:31 +0300

    Add comma to output

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,6 +14,6 @@ void print_diagsums(int *a, int size)
 		diag1 += a[i * size + i];
 		diag2 += a[i * size + (size - 1 - i)];
 	}
-	printf("%d %d\n", diag1, diag2);
+	printf("%d, %d\n", diag1, diag2);
 }
 
