6c6c26f0c3890c85a55e5a7fbdbd706080724820
Merch7x <aquamaingi@gmail.com>
2023-08-01 00:57:15 +0300

    Use index to locate first occurence

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		if (*s == c)
+		if (s[i] == c)
 		{
-			return (s);
+			return (&(s[i]));
 		}
-		s++;
+		i++;
 	}
-	return ('\0');
+	return (NULL);
 }
 
