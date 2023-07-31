8377a1408994e1931eec6098f3cbaed274e4f662
Merch7x <aquamaingi@gmail.com>
2023-07-31 12:38:23 +0300

    Copy memory address formsrc to dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+/**
+ * _memcpy - copies n bytes from memory area src to dest
+ *  @dest: memory area to copy to
+ *  @src: memory area to copy from
+ *  @n: number of bytes to copy
+ *  Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		dest[i + 1] = src[i];
+		i++;
+	}
+	return (dest);
+}
