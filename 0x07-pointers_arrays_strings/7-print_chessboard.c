d84a566e6ad28a19bfb4055532302aa08331a22d
Merch7x <aquamaingi@gmail.com>
2023-07-31 22:43:22 +0300

    Print a chessboard to stdout

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @a: arry of letters:
+ * Return: nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int j = 0, i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
