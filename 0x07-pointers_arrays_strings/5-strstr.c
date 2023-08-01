af8aa2b8e0ded25b522bb892aaf7934c39f1f95d
Merch7x <aquamaingi@gmail.com>
2023-08-01 18:01:20 +0300

    Add iteration for search string

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,11 +9,14 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i = 0, j = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	for (j = 0; needle[j] != '\0'; j++)
 	{
-		if (needle[j] == haystack[i])
+		for (i = 0; haystack[i] != '\0'; i++)
 		{
-			return (&(haystack[i]));
+			if (needle[j] == haystack[i])
+			{
+				return (&(haystack[i]));
+			}
 		}
 	}
 	return (NULL);
