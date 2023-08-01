fa5cf147e2c554c6dc49a8eed44e5243a92c811a
Merch7x <aquamaingi@gmail.com>
2023-08-01 18:03:33 +0300

    Change comparisons in if statment

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,7 +13,7 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (i = 0; haystack[i] != '\0'; i++)
 		{
-			if (needle[j] == haystack[i])
+			if (haystack[i] == needle[j])
 			{
 				return (&(haystack[i]));
 			}
