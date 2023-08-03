869460bcb2f727566c78d0d7d2e85f447bd41497
Merch7x <aquamaingi@gmail.com>
2023-08-03 19:39:15 +0300

    Result powers of x recusively

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _pow_recursion - return x raised to y
+ * @x: int to be raised
+ * @y: power to raise to
+ * Return: the raised value
+ */
+int _pow_recursion(int x, int y)
+{
+	int res = 1;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0 || x == 1)
+	{
+		return (1);
+	}
+	
+	res *= x * _pow_recursion(x, y - 1);
+	return (res);
+}
