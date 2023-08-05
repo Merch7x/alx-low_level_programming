4be96311da451536f53d0e6b90f1f140947280d5
Merch7x <aquamaingi@gmail.com>
2023-08-05 05:09:41 +0300

    Add numbers provided as args

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns the sum of positive numbers
+ * @argc: counts of arguements
+ * @argv: array of arguements
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int res = 0, i = 1, num = 0;
+
+	if (argc ==  1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		num = atoi(argv[i]);
+
+		if (num == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		if (num > 0)
+		{
+			res += num;
+			printf("%d\n", res);
+		}
+	}
+	return (0);
+}
+
