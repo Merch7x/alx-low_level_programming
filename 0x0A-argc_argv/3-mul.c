7ca8bfc424b2ff7f852d383f99b7bc42b9145bcb
Merch7x <aquamaingi@gmail.com>
2023-08-05 04:04:37 +0300

    Multiply two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+/**
+ * main - multiply arguments
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: 1 or 0
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+	int res = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", res);
+	}
+	return (0);
+}
+
+
