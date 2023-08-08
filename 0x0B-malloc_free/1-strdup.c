3a5cf985d2a3a08722f0f15717f1808077ac19a2
Merch7x <aquamaingi@gmail.com>
2023-08-09 02:10:27 +0300

    Duplicate string in new memory location

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - copies an existing string in new memory
+ * @str: string copy
+ * Return: pointer to newly allocate dspace in memory
+ */
+char *_strdup(char *str)
+{
+	int i = 0, len;
+	char *new_string;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	len = i + 1;
+
+	new_string = malloc(sizeof(char) * len);
+	for (i = 0; i <= len; i++)
+	{
+		new_string[i] = str[i];
+	}
+
+	if (sizeof(new_string) != sizeof(str))
+	{
+		return (NULL);
+	}
+	return (new_string);
+	free(new_string);
+}
