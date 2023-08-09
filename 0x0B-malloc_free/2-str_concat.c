e6b70df07e425253ac55d695509ee467a98d5302
Merch7x <aquamaingi@gmail.com>
2023-08-09 03:52:10 +0300

    Handle null pointer parameter case

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,11 +13,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = '\0';
+		s1 = "";
 	}
-	else if (s2 == NULL)
+
+	if (s2 == NULL)
 	{
-		s2 = '\0';
+		s2 = "";
 	}
 
 	while (s1[i] != '\0' && s2[j] != '\0')
