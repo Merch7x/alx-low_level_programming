07f4e4abd56fa81811bd3af69ed6c5216f60e209
Merch7x <aquamaingi@gmail.com>
2023-08-09 03:39:36 +0300

    Conacatenate two strings to new memory location

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings
+ * @s1: string to be contatenated to
+ * @s2: string to concatenate
+ * Return: pointer to new memory location
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, len = 0;
+	char *new_string;
+
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		i++;
+		j++;
+	}
+	len = i + j + 1;
+	new_string = malloc(sizeof(char) * len);
+
+	if (new_string == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		new_string[i] = s1[i];
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		new_string[i + j] = s2[j];
+	}
+	new_string[i + j] = '\0';
+	return (new_string);
+	free(new_string);
+}
