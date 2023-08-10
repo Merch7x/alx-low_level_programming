446707246a24454d34510c011545eda1cfc1a5c2
Merch7x <aquamaingi@gmail.com>
2023-08-10 18:31:42 +0300

    Allocate memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: bytes to be allocated
+ * Return: Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *size;
+
+	size = malloc(sizeof(unsigned int) * b);
+
+	if (size == NULL)
+	{
+		exit(98);
+	}
+	return (size);
+	free(size);
+}
