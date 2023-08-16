9598e4f35c905400e166b5487631a0fbcbfe3d01
Merch7x <aquamaingi@gmail.com>
2023-08-16 18:33:10 +0300

    Remove my_name reference

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 /**
- * my_name - prints a name
- * @name: name to be printed out
- * Return: Nothing
- */
-void my_name(char *name)
-{
-	printf("%s\n", name);
-}
-/**
- * print_name - prints name from pfn
+ * print_name - print name
  * @name: name to be printed
  * @f: function pointer
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f = my_name;
 	f(name);
 }
 
