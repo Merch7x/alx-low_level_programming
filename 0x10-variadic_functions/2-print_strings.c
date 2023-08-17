5cf0ff097421a1b1a21df8b30fb2bbbbe668e9ab
Merch7x <aquamaingi@gmail.com>
2023-08-18 01:33:59 +0300

    Fix Null conditional for null parametrs

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (value == NULL)
 		{
-			printf("(nil)");
+			printf("nil");
 		}
 
 		if (separator != NULL && i < n - 1)
