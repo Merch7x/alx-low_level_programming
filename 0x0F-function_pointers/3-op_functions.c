da41910da73469f5ceefa9a542751cebcaccfae5
Merch7x <aquamaingi@gmail.com>
2023-08-17 03:37:34 +0300

    Add working ops functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - adds two integers
+ * @a: integer
+ * @b: integer
+ * Return: sum of a  and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtract two integers
+ * @a: integer
+ * @b: integer
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two integers
+ * @a: integer
+ * @b: integer
+ * Return: quotient of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - checks for remeinders
+ * @a: integer
+ * @b: integer
+ * Return: remainder or zero
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
