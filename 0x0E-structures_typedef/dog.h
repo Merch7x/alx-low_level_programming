4d450199d61ce8be52802136167f987830aedbe6
Merch7x <aquamaingi@gmail.com>
2023-08-19 02:53:00 +0300

    Clean empty lines

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,18 +17,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-/**
- * struct dog_t - typedef for struct
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
- */
-typedef struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-}dog_t;
-
-
 #endif
