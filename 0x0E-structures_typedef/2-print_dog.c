283dc0c754deabb198568bcbb19cc2cfe7f1d4ee
Merch7x <aquamaingi@gmail.com>
2023-08-19 05:00:04 +0300

    Fix linter errors

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,13 +8,11 @@
  */
 void print_dog(struct dog *d)
 {
-	    if (d == NULL)
-    {
-        printf("Invalid pointer to struct dog.\n");
-        return;
-    }
-
-    printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-    printf("Age: %.6f\n", d->age);
-    printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	if (d == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.6f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
 }
