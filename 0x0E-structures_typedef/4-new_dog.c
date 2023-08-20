1873a22e1597385415c161fbcae3031339d847c8
Merch7x <aquamaingi@gmail.com>
2023-08-20 21:48:33 +0300

    Create copis of structs members

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,40 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strlen - calculates the lenth of a string
+ * @s: pointer of char type
+ * Return: an integer value
+ */
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
+/**
+ * _strcpy - copy one param to another
+ * @dest: destination parameter to copy to
+ * @src: source parameter
+ * Return: char value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+/**
  * new_dog - creates anew dog entry
  * @name: name of dog
  * @age: age of dog
@@ -9,15 +43,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog_t *dog;
+	dog_t *dog1;
 
-	dog = malloc(sizeof(struct dog_t));
-	if (dog == NULL)
+	if (owner == NULL || name == NULL || age < 0)
 	{
 		return (NULL);
 	}
-	dog->name = name;
-	dog->owner = owner;
-	dog->age = age;
-	return (dog);
+	dog1 = malloc(sizeof(dog1));
+
+	if (dog1 == NULL)
+	{
+		return (NULL);
+	}
+	dog1->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (dog1->name == NULL)
+	{
+		free(dog1);
+		return (NULL);
+	}
+	dog1->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (dog1->owner == NULL)
+	{
+		free(dog1->name);
+		free(dog1);
+		return (NULL);
+	}
+
+	dog1->name = _strcpy(dog1->name, name);
+	dog1->age = age;
+	dog1->owner = _strcpy(dog1->owner, owner);
+	return (dog1);
 }
