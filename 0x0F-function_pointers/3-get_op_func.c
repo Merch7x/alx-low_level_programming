a082d42d2e4561024730334de73fa812a2e84e46
Merch7x <aquamaingi@gmail.com>
2023-08-22 03:07:29 +0300

    Add function to select operator

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,7 @@
+/**
+ * get_op_func - selects the correct functio to be executed
+ * @s: operator passed as argument
+ * Return: A pointer to the corresponding operator
+*/
+int (*get_op_func(char *s))(int, int)
+{}
