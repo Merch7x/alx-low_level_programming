13c777e111d4ea7d337e5256633a8602b5aa71f7
Merch7x <aquamaingi@gmail.com>
2023-08-25 01:30:16 +0300

    Remove AND operator

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,7 +12,7 @@ size_t print_list(const list_t *h)
 
 	while (current != NULL)
 	{
-	if (current->str != NULL && current->len != 0)
+	if (current->str != NULL)
 	{
 	printf("[%d] %s\n", current->len, current->str);
 	}
