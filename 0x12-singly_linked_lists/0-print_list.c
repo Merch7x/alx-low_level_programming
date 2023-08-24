92ad0952a8ece149beff230dd5a2680d0cc1471f
Merch7x <aquamaingi@gmail.com>
2023-08-25 01:22:17 +0300

    Fix linter Errors

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,23 +7,23 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t i = 0;
-    const list_t *current = h;
+	size_t i = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        if (current->str != NULL && current->len != 0)
-        {
-            printf("[%d] %s\n", current->len, current->str);
-						i++;
-        }
-        else
-        {
-            printf("[0] (null)\n");
-            i++;
-        }
-        current = current->next;
-    }
-    return (i);
+	while (current != NULL)
+	{
+	if (current->str != NULL && current->len != 0)
+	{
+	printf("[%d] %s\n", current->len, current->str);
+	i++;
+	}
+	else
+	{
+	printf("[0] (null)\n");
+	i++;
+	}
+	current = current->next;
+	}
+	return (i);
 }
 
