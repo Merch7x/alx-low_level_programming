a56e07b70b3faaadc7610dbba1c37f66afc6ce37
Merch7x <aquamaingi@gmail.com>
2023-08-25 01:20:09 +0300

    Print the elements of a linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - prints the elements of a linked list
+ * @h: head file of linked list
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+    size_t i = 0;
+    const list_t *current = h;
+
+    while (current != NULL)
+    {
+        if (current->str != NULL && current->len != 0)
+        {
+            printf("[%d] %s\n", current->len, current->str);
+						i++;
+        }
+        else
+        {
+            printf("[0] (null)\n");
+            i++;
+        }
+        current = current->next;
+    }
+    return (i);
+}
+
