8e03b36dcbf2c47b97024ef3b94861c11af662ac
Merch7x <aquamaingi@gmail.com>
2023-08-25 04:37:21 +0300

    Assembly intro

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,18 @@
+extern printf
+
 section .text
- global main ;must be declared for linker (ld)
-main: ;tells linker entry point
- mov edx,len ;message length
- mov ecx,msg ;message to write
- mov ebx,1 ;file descriptor (stdout)
- mov eax,4 ;system call number (sys_write)
- int 0x80 ;call kernel
-  mov eax,1 ;system call number (sys_exit)
- int 0x80 ;call kernel
+global main
+
+main:
+    push rbp
+    mov rdi, fmt
+    mov rsi, msg
+    xor rax, rax
+    call printf
+    pop rbp
+    xor rax, rax
+    ret
+
 section .data
-msg db 'Hello, Holberton', 0xa ;our dear string
-len equ $ - msg ;length of our dear string
+msg: db "Hello, Holberton", 0
+fmt: db "%s", 10, 0
