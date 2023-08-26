3ee90c9c7a34060c166f8b725389a0b65b36cf66
Merch7x <aquamaingi@gmail.com>
2023-08-26 15:29:54 +0300

    Create 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,51 +5,37 @@
  */
 void times_table(void)
 {
-	int i = 0, j, result;
-	char c = ',', s = ' ', n = '\n';
 
-	while (i <= 9)
+	int i = 0, j = 0, result = 0;
+	char c = ',', s = ' ';
+
+	for (i = 0; i <= 9; i++)
 	{
-		j = 0;
-		while (j <= 9)
+		for (j = 0; j <= 9; j++)
 		{
 			result = i * j;
 
-			if (result <= 9)
+			if (j == 0)
 			{
 				_putchar(result + '0');
-
-				if (j < 9 )
-				{
-					if ((result < 8 || (j == 8 && i == 1)) && (i < 5 || result == 0))
-					{
-						_putchar(c);
-						_putchar(s);
-						_putchar(s);
-					}
-					else
-					{
-						_putchar(c);
-						_putchar(s);
-					}
-				}
 			}
-			else if (result > 9)
+
+			if (result >= 10)
 			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
-
-				if (j < 9)
-				{
-					_putchar(c);
-					_putchar(s);
-				}
+				_putchar(c);
+				_putchar(s);
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
 			}
-			j++;
-
+			else if (result < 10 && j != 0)
+			{
+				_putchar(c);
+				_putchar(s);
+				_putchar(s);
+				_putchar(result + '0');
+			}
 		}
-		_putchar(n);
-		i++;
+		_putchar('\n');
+
 	}
 }
-
