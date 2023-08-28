801aeff5737d1fe9670dbb99590968757286ad46
Merch7x <aquamaingi@gmail.com>
2023-08-28 10:01:59 +0300

    Add function prototype for task1

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define
+#define LISTS_H
 
 /**
  * struct listint_s singly linked list
@@ -15,6 +15,8 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+
+size_t listint_len(const listint_t *h);
 
 #endif
 
