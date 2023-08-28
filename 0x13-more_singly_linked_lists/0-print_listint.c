de8af112302f9dbc2f579070278bab41f6fe05e5
Merch7x <aquamaingi@gmail.com>
2023-08-28 10:01:14 +0300

    Prints node members of a s truct

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - prints the nodes of a linked list
+ * @h: header reference of the linked list
+ * Return: size of the nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		count++;
+		current = current->next;
+	}
+	return (count);
+}
