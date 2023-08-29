d33418842492c231303b785cefe3615653fa5e41
Merch7x <aquamaingi@gmail.com>
2023-08-29 09:06:01 +0300

    Check for empty linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,6 +10,11 @@ size_t print_listint(const listint_t *h)
 	size_t count = 0;
 	const listint_t *current = h;
 
+	if (h == NULL)
+	{
+		return (0);
+	}
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
