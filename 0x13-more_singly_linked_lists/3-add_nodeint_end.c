793d0cda8482dff4e6b043ca3104dc80105b5ae0
Merch7x <aquamaingi@gmail.com>
2023-08-29 17:54:37 +0300

    Check for one node list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -20,7 +20,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	if (*head == NULL)
 	{
-		return (NULL);
+		*head = new_node;
+		return (new_node);
 	}
 	new_node->n = n;
 	new_node->next = NULL;
