469b8b6229b77e1818f33099cf13efa876b55ca0
Merch7x <aquamaingi@gmail.com>
2023-08-29 09:37:40 +0300

    Remove check for NULL head

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -21,11 +21,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		return (NULL);
 	}
 
-	if (head == NULL)
-	{
-		*head = new_node;
-		return (*head);
-	}
 	new_node->n = n;
 	new_node->next = *head;
 	*head = new_node;
