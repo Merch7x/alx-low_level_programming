8cd4a26fb4f2c765afe909cc3051aba04eccd1e6
Merch7x <aquamaingi@gmail.com>
2023-08-29 12:34:45 +0300

    Check null head

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,18 +12,22 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node;
 	listint_t *tmp;
 
-	if (n < 0)
-	{
-		return (NULL);
-	}
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node == NULL)
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = NULL;
+	}
+	else
 	{
 		return (NULL);
 	}
-	new_node->n = n;
-	new_node->next = NULL;
+	
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	if (*head == NULL)
 	{
 		*head = new_node;
