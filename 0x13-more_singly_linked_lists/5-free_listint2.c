aa4d66005fd3b7fd85d1e5e3fa841549e28b4661
Merch7x <aquamaingi@gmail.com>
2023-08-30 02:14:59 +0300

    Fix loop

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,19 +11,19 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *tmp = *head;
 	listint_t *next;
-	listint_t **temp = head;
 
-if (temp != NULL)
-{
-	while (*head != NULL)
+	if (head == NULL)
 	{
-		next = *head;
-		free(next);
-		*head = (*head)->next;
+		exit(1);
 	}
-
-*temp = NULL;
-}
+	while (tmp != NULL)
+	{
+		next = tmp->next;
+		free(tmp);
+		tmp = next;
+	}
+	*head = NULL;
 }
 
