ff67816c454e32dd19961b084052728c9e82eef4
Merch7x <aquamaingi@gmail.com>
2023-09-01 02:43:55 +0300

    Get the value of bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * get_bit - get a bit at a specified index
+ * @n: integer to convert to binary
+ * @index: index of bit
+ * Return: value at index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index > 63)
+{
+return (-1);
+}
+n = ((n >> index) & 1);
+return (n);
+}
