47b7c52b278403b271014fbded79afc37b25ac56
Merch7x <aquamaingi@gmail.com>
2023-09-01 02:20:06 +0300

    Add function description

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_binary - outputs a decimals binary rep
+ * @n: decimal number
+ * Return: Nothing
+*/
 void print_binary(unsigned long int n)
 {
 if (n > 1)
