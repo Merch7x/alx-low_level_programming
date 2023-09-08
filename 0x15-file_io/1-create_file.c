4dece5e20cf28d0943a7a81e72f0ec95c6799011
Merch7x <aquamaingi@gmail.com>
2023-09-08 15:21:59 +0300

    Remove O_TRUNC

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,22 +12,32 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, fd2;
+	int fd;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
+	fd = open(filename, O_RDWR | O_CREAT, 00600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	fd2 = write(fd, text_content, sizeof(text_content));
-	if (fd2 == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		if (write(fd, text_content, sizeof(text_content)) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	else
+	{
+		if (write(fd, "", 0) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
