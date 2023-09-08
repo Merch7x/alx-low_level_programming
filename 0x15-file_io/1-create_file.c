b1453ddab1a3b20d92620fb019f07e1298d2485b
Merch7x <aquamaingi@gmail.com>
2023-09-08 15:17:03 +0300

    Remove redundant check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,32 +12,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, fd2;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-	if (text_content != NULL)
+	fd2 = write(fd, text_content, sizeof(text_content));
+	if (fd2 == -1)
 	{
-		if (write(fd, text_content, sizeof(text_content)) == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	else
-	{
-		if (write(fd, "", 0) == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
