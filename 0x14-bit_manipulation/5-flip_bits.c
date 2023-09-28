6e018bbfa2a8037c86e3a3fb2eabc47e9706911c
Merch7x <aquamaingi@gmail.com>
2023-09-28 10:11:09 +0300

    Evaluate flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * flip_bits - returns number of bits needed to resemble m fron n
+ * @n: number
+ * @m: number
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(const unsigned long int n, const unsigned long int m)
+{
+unsigned long int dif_in_bits = n ^ m;
+unsigned int count = 0;
+
+if (n == m)
+{
+return (0);
+}
+while (dif_in_bits)
+{
+if (dif_in_bits & 1)
+{
+count++;
+}
+dif_in_bits >>= 1;
+}
+return (count);
+}
+
+
