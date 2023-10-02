ed6ef9232f6f0f024b3d30c202042d439fd65310
Merch7x <aquamaingi@gmail.com>
2023-10-02 08:51:35 +0300

    Create and append to file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		if (write(fd, text_content, strlen(text_content)) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	else
+	{
+		if (write(fd, "", 0) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
