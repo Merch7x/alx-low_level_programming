6be18667daa05c440e4aaad3c3e3e8d69e53b1f7
Merch7x <aquamaingi@gmail.com>
2023-11-09 21:33:54 +0300

    Add function prototype for task 0

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -2,6 +2,10 @@
 #define LISTS_H
 
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -17,6 +21,8 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
     struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 
 
