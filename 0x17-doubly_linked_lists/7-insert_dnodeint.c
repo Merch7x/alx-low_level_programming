5ef6457beb327e46b47543feccbb7fdce5e92af6
Merch7x <aquamaingi@gmail.com>
2023-11-10 00:56:14 +0300

    Refactor from counter operations

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,27 +9,30 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int count = 0;
 	dlistint_t *current = *h, *new_node;
 
-	if (h == NULL || *h == NULL)
-	{
-		return (NULL);
-	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	while (current != NULL)
+	for (; idx != 1; idx--)
 	{
-		if (count == idx)
-		{
-			new_node = add_dnodeint(h, n);
-			return (new_node);
-		}
-		count++;
 		current = current->next;
+		if (current == NULL)
+			return (NULL);
 	}
 
-	new_node = add_dnodeint_end(h, n);
+	if (current->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = current;
+	new_node->next = current->next;
+	current->next->prev = new_node;
+	current->next = new_node;
+
 	return (new_node);
 }
-
-
