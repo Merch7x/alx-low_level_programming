44e458fbb11998b49e42b563131d14b74aa7ef5a
Merch7x <aquamaingi@gmail.com>
2024-03-16 18:23:19 +0300

    Update logic kidogo

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -5,38 +5,44 @@
  * @array: sorted array
  * @size: size of the array
  * @value: value to search for
- * Return: -1 if not found, index of value if found
+ * Return: -1 or index of value
  */
 int jump_search(int *array, size_t size, int value)
 {
-int flag = -1;
-int jump_factor = (int)sqrt(size);
-int low = 0, high = jump_factor;
-int i = 0;
+	int flag = -1, j = 0, i = 0;
+	int jump_factor = (int)sqrt(size);
+	int low = 0, high = jump_factor;
 
-if (array == NULL || size == 0)
-{
-	return (-1);
-}
+	if (array == NULL)
+	{
+		return (flag);
+	}
 
-while (high < (int)size && array[high] < value)
-{
-	printf("Value checked array[%d] = [%d]\n", high, array[high]);
-	low = high;
-	high += jump_factor;
+	for (i = low; i < high && i < (int)size; i += jump_factor)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (value == array[low])
+		{
+			flag = low;
+			break;
+		} else if (value > array[high])
+		{
+			low = high;
+			high = low + jump_factor;
+		}
+		else
+		{
+			printf("Value found between indexes [%d] and [%d]\n", low, high);
+			for (j = low; j <= high && j < (int)size; j++)
+			{
+				printf("Value checked array[%d] = [%d]\n", j, array[j]);
+				if (value == array[j])
+				{
+					flag = j;
+					break;
+				}
+			}
+		}
+	}
+	return (flag);
 }
-
-printf("Value found between indexes [%d] and [%d]\n", low, high);
-for (i = low; i <= high && i < (int)size; i++)
-{
-printf("Value checked array[%d] = [%d]\n", i, array[i]);
-if (array[i] == value)
-{
-	flag = i;
-	break;
-}
-}
-
-return (flag);
-}
-
