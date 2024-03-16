1a729dc11d5c0093b37d7cafba7bb791dbcdb820
Merch7x <aquamaingi@gmail.com>
2024-03-16 16:16:36 +0300

    Update logic

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,52 +1,42 @@
 #include "search_algos.h"
+
 /**
  * jump_search - searches for a value in a sorted array
  * @array: sorted array
  * @size: size of the array
  * @value: value to search for
- * Return: -1 or index of value
+ * Return: -1 if not found, index of value if found
  */
 int jump_search(int *array, size_t size, int value)
 {
-	int flag = -1, j = 0, i = 0;
-	int jump_factor = (int)sqrt(size);
-	int low = 0, high = jump_factor;
-	int jumps = 0;
+int flag = -1;
+int jump_factor = (int)sqrt(size);
+int low = 0, high = jump_factor;
+int i = 0;
 
-	if (array == NULL)
-	{
-		return (flag);
-	}
+if (array == NULL || size == 0)
+{
+	return (-1);
+}
 
-	for (i = low; i < high;)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (value == array[low])
-		{
-			flag = low;
-			break;
-		} else if (value > array[high])
-		{
-			jumps += 1;
-			low = high;
-			high = low + jump_factor;
-		}
-		else
-		{
-			printf("Value found between indexes [%d] and [%d]\n", low, high);
-			for (j = low; j <= high; j++)
-			{
-				printf("Value checked array[%d] = [%d]\n", j, array[j]);
-				if (value == j)
-				{
-					flag = j;
-					break;
-				}
-				jumps += 1;
-			}
-		}
-		i += 3;
-	}
-	return (flag);
+while (high < (int)size && array[high] < value)
+{
+	printf("Value checked array[%d] = [%d]\n", high, array[high]);
+	low = high;
+	high += jump_factor;
+}
+
+printf("Value found between indexes [%d] and [%d]\n", low, high);
+for (i = low; i <= high && i < (int)size; i++)
+{
+printf("Value checked array[%d] = [%d]\n", i, array[i]);
+if (array[i] == value)
+{
+	flag = i;
+	break;
+}
+}
+
+return (flag);
 }
 
